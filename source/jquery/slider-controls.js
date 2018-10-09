39cd46ea8d6f9415fad5992a94ff014b9ad606ef
Сергей <agrael@yandex.ru>
2018-10-09 15:29:36 +0300

    fixing

--- source/jquery/slider-controls.js
+++ source/jquery/slider-controls.js
@@ -1,20 +1,96 @@
 $(document).ready(function(){
-    $('.info__items-list').slick({
-        responsive: [
-            {
-                breakpoint: 9999,
-                settings: "unslick"
-            },
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false,
-                    fade: true,
-                    dots: true,
+    var showSlider = function() {
+        $('.info__items-list').slick({
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: true,
+                        asNavFor: '.spec__content--general, .spec__content--bowl, .spec__content--options'
+                    }
                 }
-            }
-        ]
+            ]
+        });
+        $('.spec__content--general').slick({
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: false,
+                        asNavFor: '.info__items-list, .spec__content--bowl, .spec__content--options'
+                    }
+                }
+            ]
+        });
+        $('.spec__content--bowl').slick({
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: false,
+                        asNavFor: '.info__items-list, .spec__content--general, .spec__content--options'
+                    }
+                }
+            ]
+        });
+        $('.spec__content--options').slick({
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: false,
+                        asNavFor: '.info__items-list, .spec__content--general, .spec__content--bowl'
+                    }
+                }
+            ]
+        });
+    };
+    showSlider();
+    $(window).resize(function(){
+        var $windowWidth = $(window).width();
+        if ($windowWidth < 800) {
+            showSlider();
+        }
     });
 });
+
+var s1 = document.querySelector('.info__wrapper');
+var s2 = document.querySelector('.spec__wrapper--general');
+var s3 = document.querySelector('.spec__wrapper--bowl');
+var s4 = document.querySelector('.spec__wrapper--options');
+
+function select_scroll_1(e) {
+    s2.scrollLeft = s1.scrollLeft;
+    s3.scrollLeft = s1.scrollLeft;
+    s4.scrollLeft = s1.scrollLeft;
+}
+
+s1.addEventListener('scroll', select_scroll_1, false);
